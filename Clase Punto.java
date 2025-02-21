86aeae080ff051011f7398d8091926bc15ba60a6
yaguilar130 <yosselynaguilarbernal@gmail.com>
2025-02-21 10:48:00 -0400

    Clase Punto.java

---
+++ Clase Punto.java
@@ -0,0 +1,17 @@
+class Punto {
+    private int x;
+    private int y;
+
+    public Punto(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
