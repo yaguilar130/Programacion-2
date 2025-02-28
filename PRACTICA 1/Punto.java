ca20f584607697b414badf4922eec9c3f8e80757
yaguilar130 <yosselynaguilarbernal@gmail.com>
2025-02-28 17:43:19 -0400

    Add files via upload

---
+++ PRACTICA 1/Punto.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.punto;
+
+/**
+ *
+ * @author Lenovo
+ */
+class Punto {
+    private int x;
+    private int y;
+
+    public Punto(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
