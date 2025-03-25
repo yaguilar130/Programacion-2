640e6a2640b321ecfa7e00833e693140594369d2
yaguilar130 <yosselynaguilarbernal@gmail.com>
2025-03-25 19:45:22 -0400

    Update Punto.java

--- PRACTICA 1/Punto.java
+++ PRACTICA 1/Punto.java
@@ -8,20 +8,40 @@ package com.mycompany.punto;
  *
  * @author Lenovo
  */
-class Punto {
-    private int x;
-    private int y;
+public class Punto {
+    public static void main(String[] args) {
+        class Main {
+            private double x;
+            private double y;
 
-    public Punto(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
+            public Main(double x, double y) {
+                this.x = x;
+                this.y = y;
+            }
 
-    public int getX() {
-        return x;
-    }
+            public double[] coordCartesianas() {
+                return new double[] {x, y};
+            }
 
-    public int getY() {
-        return y;
+            public double[] coordPolares() {
+                double r = Math.sqrt(x * x + y * y);
+                double theta = Math.atan2(y, x);
+                return new double[] {r, theta};
+            }
+
+            @Override
+            public String toString() {
+                return "Punto(" + x + ", " + y + ")";
+            }
+        }
+
+        Main p = new Main(3, 4);
+        double[] cartesianas = p.coordCartesianas();
+        System.out.println("Coordenadas Cartesianas: (" + cartesianas[0] + ", " + cartesianas[1] + ")");
+        
+        double[] polares = p.coordPolares();
+        System.out.println("Coordenadas Polares: (r: " + polares[0] + ", θ: " + polares[1] + ")");
+        
+        System.out.println("Representación String: " + p);
     }
 }
