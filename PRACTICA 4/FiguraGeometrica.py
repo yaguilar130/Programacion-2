de0c5594ea67aa67589e9896a4aca2496804fecc
yaguilar130 <yosselynaguilarbernal@gmail.com>
2025-03-25 20:07:42 -0400

    Update FiguraGeometrica.py

--- PRACTICA 4/FiguraGeometrica.py
+++ PRACTICA 4/FiguraGeometrica.py
@@ -1,56 +1,38 @@
 import math
 
-class Figura:
-    def area(self):
-        raise NotImplementedError("Este método debe ser implementado por subclases")
+class AreaCalculator:
+    def area(self, shape, *args):
+        if shape == 'círculo':
+            return self.area_circulo(args[0])
+        elif shape == 'rectángulo':
+            return self.area_rectangulo(args[0], args[1])
+        elif shape == 'triángulo':
+            return self.area_triangulo(args[0], args[1])
+        elif shape == 'trapecio':
+            return self.area_trapecio(args[0], args[1], args[2])
+        elif shape == 'pentágono':
+            return self.area_pentagono(args[0])
+        else:
+            return "Forma no reconocida"
 
-class Circulo(Figura):
-    def __init__(self, radio):
-        self.radio = radio
+    def area_circulo(self, radio):
+        return math.pi * radio ** 2
 
-    def area(self):
-        return math.pi * self.radio ** 2
+    def area_rectangulo(self, base, altura):
+        return base * altura
 
-class Rectangulo(Figura):
-    def __init__(self, base, altura):
-        self.base = base
-        self.altura = altura
+    def area_triangulo(self, base, altura):
+        return (base * altura) / 2
 
-    def area(self):
-        return self.base * self.altura
+    def area_trapecio(self, base_menor, base_mayor, altura):
+        return ((base_menor + base_mayor) * altura) / 2
 
-class Triangulo(Figura):
-    def __init__(self, base, altura):
-        self.base = base
-        self.altura = altura
+    def area_pentagono(self, lado):
+        return (5 * lado ** 2) / (4 * math.tan(math.pi / 5))
 
-    def area(self):
-        return (self.base * self.altura) / 2
-
-class Trapecio(Figura):
-    def __init__(self, base_menor, base_mayor, altura):
-        self.base_menor = base_menor
-        self.base_mayor = base_mayor
-        self.altura = altura
-
-    def area(self):
-        return ((self.base_menor + self.base_mayor) * self.altura) / 2
-
-class Pentagono(Figura):
-    def __init__(self, lado):
-        self.lado = lado
-
-    def area(self):
-        return (5 * self.lado ** 2) / (4 * math.tan(math.pi / 5))
-
-# Ejemplo de uso
-figuras = [
-    Circulo(5),
-    Rectangulo(4, 6),
-    Triangulo(4, 3),
-    Trapecio(3, 5, 4),
-    Pentagono(3)
-]
-
-for figura in figuras:
-    print(f"Área de la figura: {figura.area()}")
+calc = AreaCalculator()
+print("Área del círculo:", calc.area('círculo', 5))
+print("Área del rectángulo:", calc.area('rectángulo', 4, 6))
+print("Área del triángulo:", calc.area('triángulo', 4, 3))
+print("Área del trapecio:", calc.area('trapecio', 3, 5, 4))
+print("Área del pentágono:", calc.area('pentágono', 3))
